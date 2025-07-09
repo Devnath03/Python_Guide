ff600959020bdbb3d8267635f4f066ec822dc958
Devnath03 <jmdevnath.c@gmail.com>
2025-07-09 12:54:59 +0530

    ADD Indexing Print

--- main2.py
+++ main2.py
@@ -1,3 +1,7 @@
 # Create List
 fruits = [' apple', 'banana', 'grapes']
 print(fruits)
+
+print(fruits[0])  # Accessing the first element
+print(fruits[1])  # Accessing the second element
+print(fruits[2])  # Accessing the third element
