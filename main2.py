ac0db5544aa287147b3b1ea84d629c6661fc9e16
Devnath03 <jmdevnath.c@gmail.com>
2025-07-09 12:56:56 +0530

    Add Neagative Print List

--- main2.py
+++ main2.py
@@ -2,6 +2,12 @@
 fruits = [' apple', 'banana', 'grapes']
 print(fruits)
 
+# Accessing elements in the list
 print(fruits[0])  # Accessing the first element
 print(fruits[1])  # Accessing the second element
 print(fruits[2])  # Accessing the third element
+
+# Accessing elements using negative indexing
+print(fruits[-1])  # Accessing the last element
+print(fruits[-2])  # Accessing the second last element
+print(fruits[-3])  # Accessing the third last element
