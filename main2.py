8c777d089a92ee7dac28a955926ac56a093be05f
Devnath03 <jmdevnath.c@gmail.com>
2025-07-09 13:00:40 +0530

    Add Nsted Print Elemnts

--- main2.py
+++ main2.py
@@ -11,3 +11,27 @@ print(fruits[2])  # Accessing the third element
 print(fruits[-1])  # Accessing the last element
 print(fruits[-2])  # Accessing the second last element
 print(fruits[-3])  # Accessing the third last element
+
+#Nested List
+nested_list = [['apple', 'banana'], ['grapes', 'orange'], ['kiwi', 'mango']]
+print(nested_list)
+
+# Accessing elements in the nested list
+print(nested_list[0])  # Accessing the first sub-list
+print(nested_list[1])  # Accessing the second sub-list
+print(nested_list[2])  # Accessing the third sub-list
+
+# Accessing elements using negative indexing
+print(nested_list[-1])  # Accessing the last sub-list
+print(nested_list[-2])  # Accessing the second last sub-list
+print(nested_list[-3])  # Accessing the third last sub-list
+
+# Accessing elements in the nested list
+print(nested_list[0][0])  # Accessing the first element of the first
+print(nested_list[0][1])  # Accessing the second element of the first
+print(nested_list[1][0])  # Accessing the first element of the second
+print(nested_list[1][1])  # Accessing the second element of the second
+print(nested_list[2][0])  # Accessing the first element of the third    
+print(nested_list[2][1])  # Accessing the second element of the third
+print(nested_list[-1][-1])  # Accessing the last element of the last sub-list
+print(nested_list[-2][-1])  # Accessing the last element of the second
