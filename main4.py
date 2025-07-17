ac21e5638707c255b420c11b56113e811a46b68d
Devnath03 <jmdevnath.c@gmail.com>
2025-07-17 20:25:08 +0530

    Add Conditional Statements

---
+++ main4.py
@@ -0,0 +1,17 @@
+#Conditional Statements
+if 10 == 2:
+    print("This is true")
+
+#Conditional statements for Day today Example
+# If Today is Rainy Day We bring An Umbrella.
+if "Rainy" == "Rainy":
+    print("Bring an Umbrella")
+
+# If Today is Sunny Day We bring Sunglasses.
+if "Sunny" == "Sunny":  
+    print("Bring Sunglasses")
+
+# If Today is Snowy Day We bring a Jacket.
+if "Snowy" == "Snowy":  
+    print("Bring a Jacket")
+    
