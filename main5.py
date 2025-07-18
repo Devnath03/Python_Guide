df9b0a12e1f8743ebe809f5c2d14cfaccb9a715a
Devnath03 <jmdevnath.c@gmail.com>
2025-07-18 23:41:48 +0530

    Add While Calculation

--- main5.py
+++ main5.py
@@ -2,3 +2,23 @@ x = 1
 while (x <= 5):
     print("Hello")
     x = x + 1
+
+initial = 1
+
+while initial <=10:
+    print(initial)
+    initial += 1 
+
+
+n = int(input("Enter a Integer no Here : "))
+
+sum = 0
+i = 1
+while i <= n:
+    sum = sum + i
+    i = i + 1
+
+print("The sum of the first" , n , "positive integers is  : " , sum)
+
+
+    
