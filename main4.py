2b9a2aa1b4370fba0360b6f49a34a400ccc72c58
Devnath03 <jmdevnath.c@gmail.com>
2025-07-18 17:44:38 +0530

    Add Elseif

--- main4.py
+++ main4.py
@@ -1,4 +1,9 @@
 #Conditional Statements
+# Conditional statements are used to perform different actions based on different conditions.
+# They are used to execute a block of code only if a specified condition is true.
+# Example of Conditional Statements
+# If 10 is equal to 2, then print "This is true".
+# In this case, the condition is false, so nothing will be printed.
 if 10 == 2:
     print("This is true")
 
@@ -15,3 +20,18 @@ if "Sunny" == "Sunny":
 if "Snowy" == "Snowy":  
     print("Bring a Jacket")
     
+#Conditional Statements with if-else
+# Conditional statements can also include an else clause to execute a block of code when the condition is false.
+if 10 == 2:
+    print("This is true")
+else:
+    print("This is false")
+
+#Conditional Statements with if-elif-else
+# Conditional statements can also include multiple conditions using elif.
+if 10 == 2:
+    print("This is true")
+elif 10 == 10:
+    print("This is also true")
+else:
+    print("This is false")
