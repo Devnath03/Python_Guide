9dcc121854f49f79dde842c20c0430dabf5fa650
Devnath03 <jmdevnath.c@gmail.com>
2025-07-19 17:45:27 +0530

    Add Comprehension method

--- main6.py
+++ main6.py
@@ -6,15 +6,19 @@ number = [ i for i in range(1 , 11) if i % 2 == 0]
 print (number)
 
 # Initialize an empty list to store pairs
-pairs = []
+lists = []
 
 # Outer loop: i ranges from 1 to 3
 for i in range(1, 4):
     # Inner loop: r ranges from 4 to 6
     for r in range(4, 7):
         # Append the pair (i, r) to the list
-        pairs.append((i, r))
+        lists.append((i, r))
 
 # Print the final list of pairs after both loops
-print(pairs)
+print(lists)
+
+#comprehension method
+list = [(p,j) for p in range (1,4) for j in range (4,7)]
+print(list)
 
