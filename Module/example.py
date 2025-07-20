3d1f01271f9f6611f95f2da174262e5244f8ae6e
Dev <jmdevnath.c@gmail.com>
2025-07-20 14:51:11 +0530

    Add Comments

--- Module/example.py
+++ Module/example.py
@@ -1,2 +1,7 @@
-def add (a,b):
-    return a+b
+# This function takes two arguments and returns their sum
+def add(a, b):
+    # Add the two input values and return the result
+    return a + b
+
+# Example usage:
+# result = add(3, 5)  # result will be 8
--- Module/main9.py
+++ Module/main9.py
@@ -1,4 +1,8 @@
+# Import the 'example' module which contains the 'add' function
 import example
 
-sum = example.add(1,2)
+# Call the 'add' function from the 'example' module with arguments 1 and 2
+sum = example.add(1, 2)
+
+# Print the result (which should be 3)
 print(sum)
