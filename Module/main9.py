6fa4f0b15f5405248b897fc5b02f71f9d4dd924a
Dev <jmdevnath.c@gmail.com>
2025-07-20 14:57:43 +0530

    Add Comments

--- Module/main9.py
+++ Module/main9.py
@@ -1,8 +1,20 @@
-# Import the 'example' module which contains the 'add' function
+# Import the 'example' module which contains the custom 'add' function
 import example
+
+# Import the built-in 'math' module and give it an alias 'ma'
+import math as ma
+
+# Import only 'pi' from the math module and rename it as 'py'
+from math import pi as py
 
 # Call the 'add' function from the 'example' module with arguments 1 and 2
 sum = example.add(1, 2)
 
-# Print the result (which should be 3)
+# Print the result of the addition (should output 3)
 print(sum)
+
+# Print the value of pi using the math module alias
+print(ma.pi)
+
+# Print the square root of 81 using the math module alias
+print(ma.sqrt(81))
